0ae661c4c1cbba5b54220e8b7226c2f1f7338a26
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-01-27 20:25:33 -0300

    feat: integrate core with nestjs

---
+++ catalog-admin-backend/src/categories/categories.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
+import { CategoriesController } from './categories.controller';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+
+@Module({
+	imports: [SequelizeModule.forFeature([CategoryModel])],
+	controllers: [CategoriesController],
+  providers: [
+    {
+      provide: CategorySequelizeRepository,
+      useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
+      inject: [getModelToken(CategoryModel)]
+    }
+  ]
+})
+export class CategoriesModule {}
