56c320d9f333aec57864b91080d912059c3a4fc8
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-01-30 21:37:35 -0300

    feat: config module

--- catalog-admin-backend/src/app.module.ts
+++ catalog-admin-backend/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
 	imports: [
-    DatabaseModule,
-    CategoriesModule,
-    DatabaseModule
-  ],
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		CategoriesModule,
+		ConfigModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- catalog-admin-backend/src/categories/categories.controller.spec.ts
+++ catalog-admin-backend/src/categories/categories.controller.spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { CategoriesModule } from './categories.module';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from '@nestjs/config';
 
 describe('CategoriesController', () => {
 	let controller: CategoriesController;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			imports: [DatabaseModule, CategoriesModule],
+			imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
 		}).compile();
 
 		controller = module.get<CategoriesController>(CategoriesController);
